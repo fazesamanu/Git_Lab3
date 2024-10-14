a9ece9770ff51c8ab551aec599359e3ce9720de1
Samay Chourasia <2023ucp1851@mnit.ac.in>
2024-10-14 14:54:10 +0530

    Modified Dot product python file

--- DotProduct.py
+++ DotProduct.py
@@ -1,9 +1,18 @@
-
 import numpy as np
+from numba import jit, prange
 
-a = [[2, 1], [0, 3]]
-b = [[1, 1], [3, 2]]
+@jit(nopython=True, parallel=True)
+def parallel_dot(a, b):
+    result = np.zeros((len(a), len(b[0])), dtype=np.float64)
+    for i in prange(len(a)):
+        for j in range(len(b[0])):
+            for k in range(len(b)):
+                result[i, j] += a[i][k] * b[k][j]
+    return result
 
+a = np.array([[2, 1], [0, 3]])
+b = np.array([[1, 1], [3, 2]])
+result = parallel_dot(a, b)
 
-print(np.dot(a, b))
+print(result)
 
